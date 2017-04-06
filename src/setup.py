bcda633d11e1483f158a6158a788fbcdf108d75e
Steve Cox (RENCI) <scox@ip-10-1-1-10.ec2.internal>
2017-04-06 22:55:09 +0000

    pypi

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+      name = 'Green Translator',
+      packages = ['greentranslator'], # this must be the same as the name above
+      version = '0.1',
+      description = 'Green Team BioMedical Data Translator',
+      author = 'Steve Cox',
+      author_email = 'scox@renci.org',
+      url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator', # use the URL to the github repo
+      download_url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator/archive/0.1.tar.gz', # I'll explain this in a second
+      keywords = ['biomedical'], # arbitrary keywords
+      classifiers = [],
+    )
