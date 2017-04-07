a58357ab318fa4ee859145cd57d17f00962b731f
StarsCluster <scox@renci.org>
2017-04-07 18:10:01 -0400

    zeppelin autocommit

---
+++ src/greentranslator/.#l2.py
@@ -0,0 +1,1 @@
+evryscope@stars-c0.edc.renci.org.29515:1491569737
--- src/greentranslator/l2.py
+++ src/greentranslator/l2.py
@@ -1,44 +1,5 @@
 import json, pprint, requests, textwrap, time
 
-host = 'http://localhost:8998'
-data = {'kind': 'pyspark'}
-headers = {'Content-Type': 'application/json'}
-r = requests.post(host + '/sessions', data=json.dumps(data), headers=headers)
-print (r.json())
-
-session_url = host + r.headers['location']
-statements_url = session_url + '/statements'
-
-while True:
-    r = requests.get(session_url, headers=headers)
-    print (r.json ())
-    if r.json()['state'] == 'idle':
-        break
-    else:
-        time.sleep (5)
-        print ("   waiting for spark context...")
-
-data = {
-  'code': textwrap.dedent("""
-    import random
-    NUM_SAMPLES = 100000
-    def sample(p):
-      x, y = random.random(), random.random()
-      return 1 if x*x + y*y < 1 else 0
-
-    count = sc.parallelize(xrange(0, NUM_SAMPLES)).map(sample).reduce(lambda a, b: a + b)
-    print "Pi is roughly %f" % (4.0 * count / NUM_SAMPLES)
-    """)
-}
-r = requests.post(statements_url, data=json.dumps(data), headers=headers)
-
-statement_url = host + r.headers['location']
-print ('statement url: {}'.format (statement_url))
-r = None
-while True:
-    r = requests.get(statement_url, headers=headers).json ()
-    if r['state'] == 'available':
-        break
 
 pprint.pprint(r.json())
 
@@ -65,6 +26,12 @@ class LivyContext(object):
     def execute (self, code):
         data = { 'code': textwrap.dedent (code)) }
         r = requests.post(self.statements_url, data=json.dumps(self.data), headers=self.headers)
+        r = None
+        while True:
+            r = requests.get(statement_url, headers=headers).json ()
+            if r['state'] == 'available':
+                break
+        return r
 
     def close (self):
         requests.delete(self.session_url, headers=self.headers)
