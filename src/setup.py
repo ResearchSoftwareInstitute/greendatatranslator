a1f4ec37d5c1e173cc3d1e5dfc9a1a04c38a2de0
StarsCluster <scox@renci.org>
2017-04-11 17:37:05 -0400

    api

--- src/setup.py
+++ src/setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
       name = 'greentranslator',
       packages = [ 'greentranslator' ], # this must be the same as the name above
-      version = '0.4',
+      version = '0.5',
       description = 'Green Team BioMedical Data Translator',
       author = 'Steve Cox',
       author_email = 'scox@renci.org',
       url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator',
-      download_url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator/archive/0.4.tar.gz',
+      download_url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator/archive/0.5.tar.gz',
       keywords = [ 'biomedical' ],
       classifiers = [ ],
     )
