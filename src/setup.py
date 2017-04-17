07338e13fdb9d48fe574375416f7564d16f39da3
Steven Cox <scox@renci.org>
2017-04-17 12:33:51 -0400

    api version 0.7

--- src/setup.py
+++ src/setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
       name = 'greentranslator',
       packages = [ 'greentranslator' ], # this must be the same as the name above
-      version = '0.6',
+      version = '0.7',
       description = 'Green Team BioMedical Data Translator',
       author = 'Steve Cox',
       author_email = 'scox@renci.org',
       url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator',
-      download_url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator/archive/0.6.tar.gz',
+      download_url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator/archive/0.7.tar.gz',
       keywords = [ 'biomedical', 'environmental', 'exposure', 'clinical' ],
       classifiers = [ ],
     )
