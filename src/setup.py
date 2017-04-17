b4a92c7844eac50132e2653ae9d8852e60fba522
Steven Cox <scox@renci.org>
2017-04-17 14:20:24 -0400

    api version 0.10

--- src/setup.py
+++ src/setup.py
@@ -3,14 +3,14 @@ setup(
       name = 'greentranslator',
       packages = [ 'greentranslator' ], # this must be the same as the name above
       package_dir={ 'greentranslator' : 'greentranslator' },
-      package_data={ 'greentranslator' : [ 'greentranslator/query/*.sparql' ]},
-      version = '0.9',
+      package_data={ 'greentranslator' : [ 'query/*.sparql' ]},
+      version = '0.10',
       description = 'Green Team BioMedical Data Translator',
       author = 'Steve Cox',
       author_email = 'scox@renci.org',
       include_package_data=True,
       url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator',
-      download_url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator/archive/0.9.tar.gz',
+      download_url = 'https://github.com/ResearchSoftwareInstitute/greendatatranslator/archive/0.10.tar.gz',
       keywords = [ 'biomedical', 'environmental', 'exposure', 'clinical' ],
       classifiers = [ ],
     )
